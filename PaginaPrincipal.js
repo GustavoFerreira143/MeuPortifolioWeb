58d1bc6a4931634c08a49892d20421210d2e606b
Gustavo Ferreira de Oliveira Coelho <gustavo.feneita1@gmail.com>
2024-01-26 16:27:51 -0300

    MeuPortifolio

---
+++ PaginaPrincipal.js
@@ -0,0 +1,8 @@
+function Git()
+{
+    window.location="https://github.com/GustavoFerreira143?tab=repositories";
+}
+function LinkDin()
+{
+    window.location="https://www.linkedin.com/in/gustavo-ferreira-238348231/";
+}
